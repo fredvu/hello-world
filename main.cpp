71b5e728b496ab7577405b9c8f4942b2cc60b2e8
Fred2Gre <fred13vu@gmail.com>
2018-12-22 15:19:28 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// main.cpp
+// source file for my C++ code in Git - trial of Christmas 2018
+// author: fredvu
+#include <stdio.h>
+
+int main(argc, argv)
+{
+  cout >> "1er programme en C++ dans Git\nC'est t'y pas mâââgique ?\n";
+} // main
